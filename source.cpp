c82911b263f22221860f43b1e2136d4174634fa0
max <ajvwolf@gmail.com>
2025-11-10 10:30:37 -0500

    created init() and close() functions for the SDL window.

--- source.cpp
+++ source.cpp
@@ -1,40 +1,68 @@
+#include <cmath>
 #include <iostream>
 #include <SDL3/SDL.h>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main() {
-	SDL_Window* window = NULL;
-	SDL_Surface* screenSurface = NULL;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+/*
+	Starts up SDL and creates a window.
+	Returns the success/fail state of its actions.
+*/
+bool init() {
+	bool success = true;
 
 	if (!SDL_Init(SDL_INIT_VIDEO)) {
-		std::cerr << "SDL failed to initalize! SDL_Error: " << SDL_GetError() << std::endl;
+		SDL_Log("SDL failed to initialize! SDL_Error: %s\n", SDL_GetError());
+		success = false;
 	}
 	else {
-		window = SDL_CreateWindow("SDL Testing", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+		window = SDL_CreateWindow("Raycaster", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
 		if (window == NULL) {
-			std::cerr << "Window failed to create! SDL_Error: " << SDL_GetError() << std::endl;
+			SDL_Log("Window failed to create! SDL_Error: %s\n", SDL_GetError());
+			success = false;
 		}
 		else {
-			screenSurface = SDL_GetWindowSurface(window);
-			SDL_FillSurfaceRect(screenSurface, NULL, SDL_MapSurfaceRGB(screenSurface, 0xFF, 0xFF, 0xFF));
-			SDL_UpdateWindowSurface(window);
+			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		}
+	}
 
-			SDL_Event e;
-			bool quit = false;
-			while (quit == false) {
-				while (SDL_PollEvent(&e)) {
-					if (e.type == SDL_EVENT_QUIT) {
-						quit = true;
-					}
+	SDL_Log("SDL initialized successfully.\n");
+	return success;
+}
+
+/*
+	Cleans things up and shuts down SDL.
+*/
+void close() {
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	renderer = NULL;
+	window = NULL;
+
+	SDL_Quit();
+	SDL_Log("Quitting...\n");
+}
+
+int main() {
+	if (init()) {
+		bool quit = false;
+		SDL_Event e;
+		
+		// main running loop
+		while (!quit) {
+			while (SDL_PollEvent(&e)) {
+				if (e.type == SDL_EVENT_QUIT) {
+					quit = true;
 				}
 			}
 		}
 	}
 
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+	close();
 
 	return 0;
 }
